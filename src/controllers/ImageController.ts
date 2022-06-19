5a147b01052bcc218ad6e0953f44ba0fb2767bce
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-06-19 17:36:03 +0200

    Initial image fetching

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import path from 'path';
 
 class ImageController {
     async index(req: express.Request, res: express.Response) {
         try {
-            return res.status(200).json({
-                message: 'Ok'
-            });
+            if (!Object.keys(req.query).length) {
+                return res.status(200).json({
+                    message:
+                        'This is the image api, please add a query with the file name and possibly the desired width and height'
+                });
+            }
+            if (!req.query.fileName) {
+                return res.status(404).json({
+                    message: 'No file name was set'
+                });
+            }
+            const file = path.normalize(
+                `${__dirname}/../../assets/images/${req.query.fileName}`
+            );
+            return res.status(200).sendFile(file);
         } catch (err) {
             return res.status(500).json({
                 message: 'Internal Server Error',
