4f01df1beeeec3286cd49ab35898bac2cc6beb6f
Ahmed Saeed <87494410+ahmedsaeed195@users.noreply.github.com>
2022-06-20 21:38:51 +0200

    abstracted imageprocessor method

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -1,5 +1,36 @@
 import express from 'express';
+import sharp from 'sharp';
 import path from 'path';
+import cache from '../config/node_cache';
+
+const imageProcessor = async (
+    fileName: string,
+    width: string,
+    height: string
+): Promise<string | unknown> => {
+    try {
+        const filePathLink = path.normalize('assets/images/');
+        const fileOutPathLink = path.normalize('assets/thumbs/');
+        const image = sharp(filePathLink + fileName);
+        const metaData = await image.metadata();
+        console.log('inside processor');
+        const dimensions = {
+            width: parseInt(width) || metaData.width,
+            height: parseInt(height) || metaData.height
+        };
+
+        const imageTarget = `${dimensions.width}-${dimensions.height}-${fileName}`;
+        const info = await image
+            .resize({ width: dimensions.width, height: dimensions.height })
+            .jpeg()
+            .toFile(fileOutPathLink + imageTarget);
+        console.log(info);
+        return imageTarget;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
 
 class ImageController {
     async index(req: express.Request, res: express.Response) {
@@ -15,9 +46,29 @@ class ImageController {
                     message: 'No file name was set'
                 });
             }
-            const file = path.normalize(
-                `${__dirname}/../../assets/images/${req.query.fileName}`
+            const query = {
+                fileName: req.query.fileName as unknown as string,
+                width: req.query.width as unknown as string,
+                height: req.query.height as unknown as string
+            };
+            const target = await imageProcessor(
+                query.fileName,
+                query.width,
+                query.height
             );
+            console.log('Type of target : ' + typeof target);
+            if (typeof target === 'object') {
+                return res.status(404).json({
+                    message: 'Image was not found!',
+                    error: target
+                });
+            }
+            const file = path.normalize(
+                `${__dirname}/../../assets/thumbs/${target}`
+            );
+            console.log('before response');
+            console.log(cache.has(req.originalUrl));
+            cache.set(req.originalUrl, file, 500);
             return res.status(200).sendFile(file);
         } catch (err) {
             return res.status(500).json({
@@ -27,4 +78,6 @@ class ImageController {
         }
     }
 }
-export default new ImageController();
+export default ImageController;
+
+export { imageProcessor };
